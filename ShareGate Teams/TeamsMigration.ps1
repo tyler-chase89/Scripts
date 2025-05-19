b90f69523638353c4aa61a81bb4d8675d5b28b70
tyler-chase89 <tyler.chase89@outlook.com>
2025-05-19 08:30:39 -0400

    Upload Teams Migration Script

---
+++ ShareGate Teams/TeamsMigration.ps1
@@ -0,0 +1,13 @@
+﻿# Import the required modules
+Import-Module sharegate
+$Teams = import-csv C:\ShareGate\Teamssharegate.csv
+
+# Connect to the source and destination tenants
+$SourceTenant = Connect-Tenant -Domain -DisableSSO -Browser
+$destinationTenant = Connect-Tenant -Domain  -Browser
+
+$TeamNames = $teams.SrcDisplayName -join ',' -split ','
+
+# Bulk migrate all teams at once
+$TeamObjects = Get-Team -Name $TeamNames -Tenant $SourceTenant -AllowMultiple
+Copy-Team -Team $TeamObjects -DestinationTenant $destinationTenant
